9fbcc77b9657e482bb09da534f6aadac491012e7
PZhannet <p.zhannet@gmail.com>
2022-12-29 22:03:36 +0200

    PriceFree.spec update

--- tests/priceFree.spec.js
+++ tests/priceFree.spec.js
@@ -5,5 +5,5 @@ test('ID 5 Check the price of a book is €0.00 when the "Read for free" button 
   const checkPriceMasteringRedmine2Edition= new CheckPriceMasteringRedmine2Edition(page);
   await checkPriceMasteringRedmine2Edition.clickBuyItOnline();
   await checkPriceMasteringRedmine2Edition.clickReadForFree();
-  await expect(checkPriceMasteringRedmine2Edition.priceFree).toHaveText('€0.00');  
+  await expect(checkPriceMasteringRedmine2Edition.priceFree).toHaveText('$0.00');  
 });
